89e1adf9af02b7842824a94aabb2bf4bb9b04557
vicente@jdalmau.es <vicente@jdalmau.es@2480690e-8b93-4946-9c88-c6c64152b314>
2008-06-04 11:02:37 +0000

    Añadida la gráfica de evolución lambda vs. omega
    git-svn-id: svn://localhost/tmp/simuib08/trunk@24 2480690e-8b93-4946-9c88-c6c64152b314

--- src/main.c
+++ src/main.c
@@ -10,22 +10,24 @@
 
 int main(int argc, char *argv[]) {
 	
-	if (argc != 4) {
+	if (argc != 5) {
 		printf("main.main(): ERROR there's at least one missing parameter\n");
-		printf("main.main(): Usage: simulator lambda_value replication_time debug(0/1)\n");
+		printf("main.main(): Usage: simulator lambda_value replication_time min_reps_number debug(0/1)\n");
 		exit(EXIT_FAILURE);
 	}
-	DEBUG = (strcmp(argv[3], "1")==0)?true:false;
+	DEBUG = (strcmp(argv[4], "1")==0)?true:false;
 	
 	// inicializacion del simulador
 	simulatorInit(atof(argv[1]), atof(argv[2]));
 
 	// Aplicacion del metodo secuencial con error relativo		
     
-    // Como minimo ejecutamos dos replicas
-	replicate();
-	replicate();
-	int n = 2;
+    // Numero minimo de replicas que ejecutamos
+    int i;
+    for (i=0; i<atoi(argv[3]); i++)
+        replicate();
+
+	int n = atoi(argv[3]);
 	
 	while (largeRelativeError(n)) {
 		replicate();
